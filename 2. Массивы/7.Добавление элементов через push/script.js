a49a0ab110164efe419c0f59f5bfe618e5ab8303
WildMrDeFold <wildmrdefox@gmail.com>
2023-01-22 22:24:43 +0700

    Добавление элементов через push

---
+++ 2. Массивы/7.Добавление элементов через push/script.js
@@ -0,0 +1,14 @@
+// Заполните массив числами 1, 2 и 3.
+let arr = [];
+arr.push(1);
+arr.push(2);
+arr.push(3);
+console.log(arr)
+
+
+// Пусть дан такой массив:
+// let arr1 = [1, 2, 3];
+// Добавьте ему в конец элементы 4 и 5.
+let arr1 = [1, 2, 3];
+arr1.push(4, 5);
+console.log(arr1)
