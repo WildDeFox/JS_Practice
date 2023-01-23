65d0172ad4d4d1a1c9666705b5535752b9545ae7
WildMrDeFold <wildmrdefox@gmail.com>
2023-01-24 06:38:44 +0700

    Тернарный оператор

---
+++ 4. Условия/7.Тернарный оператор/script.js
@@ -0,0 +1,15 @@
+// Перепишите следующий код через тернарный оператор:
+
+// let num = 1;
+// let res;
+
+// if (num >= 0) {
+// 	res = '1';
+// } else {
+// 	res = '2';
+// }
+
+// console.log(res);
+let num = 1;
+let res = num >= 0? '1': '2';
+console.log(res)
