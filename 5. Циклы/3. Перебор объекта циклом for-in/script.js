ccc13882319b73865c24386b3ae61dde0c3ed891
WildMrDeFold <wildmrdefox@gmail.com>
2023-01-24 07:25:25 +0700

    Перебор объекта циклом for-in

---
+++ 5. Циклы/3. Перебор объекта циклом for-in/script.js
@@ -0,0 +1,12 @@
+// Выведите в консоль все ключи следующего объекта:
+let obj = {x: 1, y: 2, z: 3};
+
+for (let key in obj) {
+  console.log(key);
+}
+
+
+// Выведите в консоль все элементы следующего объекта:
+for (let key in obj) {
+  console.log(obj[key]);
+}
