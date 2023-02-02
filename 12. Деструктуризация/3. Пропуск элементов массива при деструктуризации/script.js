eadc852a8a6edbbd260c0f1682b652e633bf1a8b
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-02 10:26:50 +0700

    Пропуск элементов массива при деструктуризации

---
+++ 12. Деструктуризация/3. Пропуск элементов массива при деструктуризации/script.js
@@ -0,0 +1,8 @@
+// В следующем коде части массива записываются в соответствующие переменные:
+
+let arr = ['John', 'Smit', 'development', 'programmer', 2000];
+
+// let department = arr[2];
+// let position   = arr[3];
+// Переделайте этот код через деструктуризацию согласно изученной теории.
+let [, , department, programmer, ] = arr;
