cd0ff0934fdc5b01a98de8a14723dfba7dfea4f0
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-03 10:08:39 +0700

    Работа с объектом Date

---
+++ 13. Время/1. Работа с объектом Date/script.js
@@ -0,0 +1,10 @@
+let date = new Date();
+
+// Выведите на экран текущий день.
+console.log(date.getDate());
+
+// Выведите на экран текущий месяц.
+console.log(date.getMonth())
+
+// Выведите на экран текущий год.
+console.log(date.getFullYear())
