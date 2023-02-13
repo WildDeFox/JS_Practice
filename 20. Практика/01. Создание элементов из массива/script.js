56ec799cb342c2ea48a5648563b8db52420b7f39
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-13 17:49:36 +0700

    Создание элементов из массива

---
+++ 20. Практика/01. Создание элементов из массива/script.js
@@ -0,0 +1,13 @@
+let parent = document.querySelector('#parent');
+let arr = [1, 2, 3, 4, 5];
+
+for (let elem of arr) {
+  let p = document.createElement('p');
+  p.textContent = elem;
+
+  p.addEventListener('click', function() {
+    this.textContent++
+  })
+
+  parent.appendChild(p);
+}
