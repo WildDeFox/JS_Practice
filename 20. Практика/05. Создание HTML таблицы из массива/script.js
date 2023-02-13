5fcca197b8bab50ebf1f21e31870292cb4dcf9e4
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-13 18:32:30 +0700

    Создание HTML таблицы из массива

---
+++ 20. Практика/05. Создание HTML таблицы из массива/script.js
@@ -0,0 +1,15 @@
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
+let table = document.querySelector('#parent');
+
+for(let subArr of arr) {
+  let tr = document.createElement('tr');
+
+  for (let elem of subArr) {
+    let td = document.createElement('td');
+    td.textContent = elem ** 2;
+
+    tr.appendChild(td)
+  }
+
+  table.appendChild(tr)
+}
