4bd947e5c947dcaa32bdd87c56c6c6f97a948abf
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-16 00:55:30 +0700

    Функции для работы с группой DOM элементов

---
+++ 21. Функции и DOM/02. Функции для работы с группой DOM элементов/script.js
@@ -0,0 +1,10 @@
+function appendText(selector, text) {
+  let elems = document.querySelectorAll(selector);
+
+  for(let elem of elems) {
+    elem.textContent += text;
+  }
+}
+
+appendText('.elem', 'Hello ');
+appendText('.elem', 'world')
