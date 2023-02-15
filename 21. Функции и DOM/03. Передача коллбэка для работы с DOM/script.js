2d775fab6e324cf25c34a4587925b678c32e605c
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-16 01:11:28 +0700

    Передача коллбэка для работы с DOM

---
+++ 21. Функции и DOM/03. Передача коллбэка для работы с DOM/script.js
@@ -0,0 +1,12 @@
+function forEach(selector, func) {
+  let elems = document.querySelectorAll(selector);
+
+  for(let elem of elems) {
+    func(elem)
+  }
+}
+
+
+forEach('.elem', function(elem) {
+  elem.textContent += '!';
+})
