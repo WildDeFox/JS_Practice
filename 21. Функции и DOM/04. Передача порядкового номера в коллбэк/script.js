4c975d8993fe7fc254685b6eab00107a6f1fc9c6
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-16 01:16:53 +0700

    Передача порядкового номера в коллбэк

---
+++ 21. Функции и DOM/04. Передача порядкового номера в коллбэк/script.js
@@ -0,0 +1,13 @@
+function forEach(selector, func) {
+  let elems = document.querySelectorAll(selector);
+
+  for(let i = 0; i < elems.length; i++) {
+    func(elems[i], i)
+  }
+}
+
+
+
+forEach('.elem', function(elem, index) {
+  elem.textContent += index;
+})
