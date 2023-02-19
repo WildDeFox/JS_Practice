75b9cd30c302169a61356677f7b26dc709d1c7af
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-19 19:10:22 +0700

    Изменение данных в JSON

---
+++ 3. Что-то посложнее/02. Коллекции/16. Изменение данных в JSON/script.js
@@ -0,0 +1,7 @@
+// Дан следующий JSON:
+// Добавьте в конец строки еще одного юзера.
+let json = '["user1","user2","user3","user4","user5"]';
+let arr = JSON.parse(json);
+arr.push('user6');
+let res = JSON.stringify(arr)
+console.log(res)
