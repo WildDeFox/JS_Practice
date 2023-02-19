6c1c812588192ccfd007a9152af935d790ada6c8
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-19 10:29:39 +0700

    Перебор коллекций Map циклом

---
+++ 3. Что-то посложнее/02. Коллекции/05. Перебор коллекций Map циклом/script.js
@@ -0,0 +1,16 @@
+let map = new Map;
+
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+
+map.set(arr1, 'data1');
+map.set(arr2, 'data2');
+
+console.log(map.get(arr1));
+console.log(map.get(arr2));
+
+
+for (let [key, value] of map) {
+  console.log(key);
+  console.log(value);
+}
