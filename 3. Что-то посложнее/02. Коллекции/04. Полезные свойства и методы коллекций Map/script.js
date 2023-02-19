c94019da2a30a3aaff9b573c97f7e86c313e35d6
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-19 10:25:40 +0700

    Полезные свойства и методы коллекций Map

---
+++ 3. Что-то посложнее/02. Коллекции/04. Полезные свойства и методы коллекций Map/script.js
@@ -0,0 +1,13 @@
+let map = new Map;
+
+// Размер коллекции
+console.log(map.size);
+
+// Проверка наличия ключа
+console.log(map.has(arr1));
+
+// Удаление по ключу
+console.log(map.delete(arr1));
+
+// Очистка коллекции
+console.log(map.clear)
