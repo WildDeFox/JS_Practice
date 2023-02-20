7a71713c32594e7b5e481b7d32847f7a3a59c907
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-20 16:04:03 +0700

    Сохранение и получение данных

---
+++ 3. Что-то посложнее/03. Хранилища/01. Сохранение и получение данных/script.js
@@ -0,0 +1,8 @@
+localStorage.setItem('key1', '1')
+localStorage.setItem('key2', '2')
+localStorage.setItem('key3', '3')
+
+let str = localStorage.getItem('key1');
+let str1 = localStorage.getItem('key2');
+let str2 = localStorage.getItem('key3');
+console.log(str, str1, str2)
