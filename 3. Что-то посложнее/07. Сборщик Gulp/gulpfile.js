5e9893e8349ea6ff784436c5ffcd048318d28688
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-22 15:39:44 +0700

    Публичные задачи в Gulp

--- 3. Что-то посложнее/07. Сборщик Gulp/gulpfile.js
+++ 3. Что-то посложнее/07. Сборщик Gulp/gulpfile.js
@@ -5,4 +5,31 @@ function task(cb) {
   cb();
 }
 
+// Создайте задачу, выводящую в консоль текущий день.
+function taskDay(cb) {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  console.log(dd);
+  cb();
+}
+
+// Создайте задачу, выводящую в консоль текущий месяц.
+function taskMonth(cb) {
+  let today = new Date();
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  console.log(mm);
+  cb();
+}
+
+// Создайте задачу, выводящую в консоль текущий год.
+function taskYear(cb) {
+  let today = new Date();
+  let yyyy = today.getFullYear();
+  console.log(yyyy);
+  cb();
+}
+
 exports.default = task;
+exports.taskDay = taskDay;
+exports.taskMonth = taskMonth;
+exports.taskYear = taskYear;
