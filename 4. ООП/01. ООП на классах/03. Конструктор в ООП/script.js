5100b2ebee4ba81e2e05c99fe0819e972501fd0f
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-25 00:26:32 +0700

    Свойства через параметры конструктора

--- 4. ООП/01. ООП на классах/03. Конструктор в ООП/script.js
+++ 4. ООП/01. ООП на классах/03. Конструктор в ООП/script.js
@@ -1,8 +1,27 @@
 // Сделайте конструктор класса Employee.
 class Employee {
   constructor(name, salary) {
-    console.log(name + ' ' + salary)
+    this.name = name;
+    this.salary = salary;
   }
+
+  // Сделайте метод, который будет выводить имя работника.
+  showName() {
+    console.log('Вас зовут: ' + this.name)
+  }
+
+  // Сделайте метод, который будет выводить зарплату работника.
+  showSalary() {
+    console.log('Ваша зарплата: ' + this.salary)
+  }
+
+  // Сделайте метод, который будет увеличивать зарплату работника на 10%.
+  appSalary() {
+    this.salary = this.salary * 1.10}
 }
 
-let emp1 = new Employee('jhon', 2000);
+let emp1 = new Employee('Jhon', 2000);
+emp1.showName();
+emp1.showSalary();
+emp1.appSalary();
+emp1.showSalary()
