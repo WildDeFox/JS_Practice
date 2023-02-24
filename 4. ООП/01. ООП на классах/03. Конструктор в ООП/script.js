39bebea3b14cb828a478d484ed5299eef13e4e27
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-25 00:21:59 +0700

    Параметры в конструкторе

--- 4. ООП/01. ООП на классах/03. Конструктор в ООП/script.js
+++ 4. ООП/01. ООП на классах/03. Конструктор в ООП/script.js
@@ -1,6 +1,8 @@
 // Сделайте конструктор класса Employee.
 class Employee {
-  constructor() {
-    console.log('+++')
+  constructor(name, salary) {
+    console.log(name + ' ' + salary)
   }
 }
+
+let emp1 = new Employee('jhon', 2000);
