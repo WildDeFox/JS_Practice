4d4e23b90bde57b0ce60abbd82993701a20d2a80
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-24 15:12:44 +0700

    Свойства объектов в ООП

--- 4. ООП/01. ООП на классах/01. Классы и объекты в ООП/script.js
+++ 4. ООП/01. ООП на классах/01. Классы и объекты в ООП/script.js
@@ -7,3 +7,15 @@ class Employee {
 // Создайте объект класса Employee и выведите его в консоль.
 let employer = new Employee;
 console.log(employer);
+
+
+// В объект класса Employee запишите свойства name, age и salary.
+employer.name = 'Jhon';
+employer.age = '27';
+employer.salary = 2700;
+
+
+// Получите данные из записанных свойств и выведите их на экран.
+console.log(employer.name);
+console.log(employer.age);
+console.log(employer.salary)
