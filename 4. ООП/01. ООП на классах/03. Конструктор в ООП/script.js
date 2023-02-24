f96783b4b47e35017aba1d93861187b7acbd8542
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-25 00:33:11 +0700

    Приватные свойства в ООП

--- 4. ООП/01. ООП на классах/03. Конструктор в ООП/script.js
+++ 4. ООП/01. ООП на классах/03. Конструктор в ООП/script.js
@@ -1,27 +1,33 @@
 // Сделайте конструктор класса Employee.
 class Employee {
-  constructor(name, salary) {
-    this.name = name;
-    this.salary = salary;
+  // В классе Employee сделайте три приватных свойства: имя, зарплату и возраст.
+  #name;
+  #salary;
+  #age;
+  constructor(name, salary, age) {
+    this.#name = name;
+    this.#salary = salary;
+    this.#age = age;
   }
 
-  // Сделайте метод, который будет выводить имя работника.
-  showName() {
-    console.log('Вас зовут: ' + this.name)
+  show() {
+    console.log('Вас зовут: ' + this.#name + ', вам ' + this.#age + ' лет. Ваша ЗП составляет: ' + this.#salary)
   }
 
-  // Сделайте метод, который будет выводить зарплату работника.
-  showSalary() {
-    console.log('Ваша зарплата: ' + this.salary)
-  }
+  // // Сделайте метод, который будет выводить имя работника.
+  // showName() {
+  //   console.log('Вас зовут: ' + this.name)
+  // }
 
-  // Сделайте метод, который будет увеличивать зарплату работника на 10%.
-  appSalary() {
-    this.salary = this.salary * 1.10}
+  // // Сделайте метод, который будет выводить зарплату работника.
+  // showSalary() {
+  //   console.log('Ваша зарплата: ' + this.salary)
+  // }
+
+  // // Сделайте метод, который будет увеличивать зарплату работника на 10%.
+  // appSalary() {
+  //   this.salary = this.salary * 1.10}
 }
 
-let emp1 = new Employee('Jhon', 2000);
-emp1.showName();
-emp1.showSalary();
-emp1.appSalary();
-emp1.showSalary()
+let emp1 = new Employee('Jhon', 2000, 25);
+emp1.show()
