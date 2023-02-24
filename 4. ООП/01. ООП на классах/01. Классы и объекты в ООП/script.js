99e0d3bdcb877b5779a8045f8fcd709fa608c9a2
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-24 15:16:56 +0700

    Несколько объектов одного класса в ООП

--- 4. ООП/01. ООП на классах/01. Классы и объекты в ООП/script.js
+++ 4. ООП/01. ООП на классах/01. Классы и объекты в ООП/script.js
@@ -9,13 +9,32 @@ let employer = new Employee;
 console.log(employer);
 
 
+// Создайте несколько объектов класса Employee.
+let emp1 = new Employee;
+let emp2 = new Employee;
+
+
 // В объект класса Employee запишите свойства name, age и salary.
 employer.name = 'Jhon';
 employer.age = '27';
 employer.salary = 2700;
 
 
+// Запишите в свойство каждого работника его имя и зарплату.
+emp1.name = "Igor";
+emp1.age = "26";
+emp1.salary = 3000;
+
+emp2.name = "Smit";
+emp2.age = "26";
+emp2.salary = 1000;
+
+
 // Получите данные из записанных свойств и выведите их на экран.
 console.log(employer.name);
 console.log(employer.age);
-console.log(employer.salary)
+console.log(employer.salary);
+
+
+// Выведите на экран сумму зарплат созданных вами работников.
+console.log(employer.salary + emp1.salary + emp2.salary);
