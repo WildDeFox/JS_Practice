42c4b69b7bde7bc7ba47f6c53790d170da73d8a8
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-26 16:47:21 +0700

    Методы потомка в ООП

--- 4. ООП/02. Наследование/01. Наследование классов/script.js
+++ 4. ООП/02. Наследование/01. Наследование классов/script.js
@@ -9,9 +9,17 @@ class User {
 }
 
 class Employee extends User {
+  setSalary(salary) {
+    this.salary = salary;
+  }
 
+  getSalary() {
+    return this.salary;
+  }
 }
 
 let emp = new Employee;
 emp.setName('Nik');
 console.log(emp.getName())
+emp.setSalary(20000);
+console.log(emp.getSalary())
