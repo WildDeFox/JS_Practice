4dbea101fb2bdf596697569d0db4bdbbc138990e
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-26 16:45:18 +0700

    Наследование публичных методов

--- 4. ООП/02. Наследование/01. Наследование классов/script.js
+++ 4. ООП/02. Наследование/01. Наследование классов/script.js
@@ -1,7 +1,17 @@
 class User {
+  setName(name) {
+    this.name = name;
+  }
 
+  getName() {
+    return this.name
+  }
 }
 
 class Employee extends User {
-  
+
 }
+
+let emp = new Employee;
+emp.setName('Nik');
+console.log(emp.getName())
