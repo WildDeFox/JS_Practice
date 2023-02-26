448590993169b7bc4e1ff53e98a641b195bf5d35
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-26 16:38:03 +0700

    Манипуляция объектами в классах

--- 4. ООП/01. ООП на классах/07. Массивы объектов в ООП/script.js
+++ 4. ООП/01. ООП на классах/07. Массивы объектов в ООП/script.js
@@ -31,3 +31,44 @@ let emps = [
 for(let emp of emps) {
   console.log(emp.getName())
 }
+
+
+// Сделайте класс EmployeesCollection, который будет содержать массив работников.
+class EmployeesCollection {
+  #employees;
+
+  constructor() {
+    this.#employees = [];
+  }
+
+  // Сделайте в этом классе метод для добавления нового работника.
+  add(employee) {
+    this.#employees.push(employee);
+  }
+
+  // Сделайте в этом классе метод для вывода всех работников.
+  show() {
+    for(let emp of this.#employees) {
+      console.log(emp.getName());
+    }
+  }
+
+  // Сделайте в этом классе метод для расчета суммарной зарплаты всех работников.
+  sumSalary() {
+    let sum = 0;
+    for(let emp of this.#employees) {
+      sum += Number(emp.getSalary())
+    }
+    return sum
+  }
+
+}
+let colloection = new EmployeesCollection;
+let emp1 = new Employee('Rick', 20, 20000);
+let emp2 = new Employee('Morty', 15, 2000);
+let emp3 = new Employee('Djeri', 40, 40);
+colloection.add(emp1);
+colloection.add(emp2);
+colloection.add(emp3);
+colloection.show()
+console.log(colloection.sumSalary())
